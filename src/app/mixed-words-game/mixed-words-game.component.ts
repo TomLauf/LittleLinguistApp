50f59ea39b387f52504910699c44b14a3849dd90
Matan <matanyahalomi6@gmail.com>
2024-08-19 19:40:34 +0300

    fixed same word

--- src/app/mixed-words-game/mixed-words-game.component.ts
+++ src/app/mixed-words-game/mixed-words-game.component.ts
@@ -76,14 +76,19 @@ export class MixedWordsGameComponent {
     }
   }
 
-  shuffleString(str: string) {
-    let arr = str.split('');
-    for (let i = arr.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [arr[i], arr[j]] = [arr[j], arr[i]];
+  shuffleString(str: string): string {
+    let shuffled = str;
+
+    while (shuffled === str) {
+      let arr = str.split('');
+      for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+      }
+      shuffled = arr.join('');
     }
 
-    return arr.join('');
+    return shuffled;
   }
 
   resetInput() {
